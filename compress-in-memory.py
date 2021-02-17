3874ff18167e35883987fbca158103bb9f14b643
Pietro Iglio <igliop@gmail.com>
2021-02-17 08:27:59 +0100

    .

---
+++ compress-in-memory.py
@@ -0,0 +1,17 @@
+import zlib
+import binascii
+
+# More examples https://pymotw.com/3/zlib/index.html
+
+original_data = b'This is the original text.'
+print('Original     :', len(original_data), original_data)
+
+compressed = zlib.compress(original_data)
+print('Compressed   :', len(compressed),
+      binascii.hexlify(compressed))
+
+decompressed = zlib.decompress(compressed)
+print('Decompressed :', len(decompressed), decompressed)
+
+
+
