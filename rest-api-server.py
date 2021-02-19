9641d6d248cd8d4f439d7479784cc991fb8a11f1
Pietro Iglio <igliop@gmail.com>
2021-02-19 16:53:51 +0100

    .

--- rest-api-server.py
+++ rest-api-server.py
@@ -1,5 +1,8 @@
 # pip install flask
 # pip install connexion
+#
+# Optional:
+# pip install connexion[swagger-ui]   >>> enables http://127.0.0.1:5000/api/ui to access the swagger
 
 # Run this and open: http://127.0.0.1:5000/api/people
 # The service is defined in swagger.yml, the implementation is in rest-service-people.py
@@ -13,7 +16,6 @@ app = connexion.App(__name__, specification_dir='./')
 
 # Read the swagger.yml file to configure the endpoints
 app.add_api('swagger.yml')
-
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
